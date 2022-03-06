656336e5402b895f9ba0d543173b7454d3d801d8
Metin Ağaoğlu <metnagaoglu@gmail.com>
2022-03-06 12:03:33 +0300

    [feat] Add socket.io to the project to dispatch the scoreboard to clients.

---
+++ websocket/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server,{
+	cors: {
+		credentials: true
+	}
+});
+
+const { createAdapter } = require("@socket.io/redis-adapter");
+const { createClient } = require("redis");
+
+const redisConnectionString = process.env.REDIS_CONN_STRING || 'redis://redis:6379';
+const pubClient = createClient({
+	url: redisConnectionString
+});
+
+const subClient = pubClient.duplicate();
+
+Promise.all([pubClient.connect(), subClient.connect()]).then(() => {
+	io.adapter(createAdapter(pubClient, subClient));
+	const port = process.env.PORT || 8000;
+	io.listen(port);
+
+	const leaderboard_nsp = io.of("/leaderboard");
+
+	/**
+	 * Leaderboard namespace
+	 */
+	leaderboard_nsp.on("connection", socket => {
+
+	});
+
+	//Root namescape
+	io.on('connection', (socket) => {
+		socket.on('on.change.leaderboard',(payload) => {
+			leaderboard_nsp.emit('leaderboard',payload);
+		});
+	});	  
+
+
+});
