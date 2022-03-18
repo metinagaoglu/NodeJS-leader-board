a1f453ae6c3b1e0fd7c5dce584361fd65dea75ef
Metin Ağaoğlu <metnagaoglu@gmail.com>
2022-03-18 21:47:43 +0300

    [feat] Add and implement express-helmet

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 8080
+const helmet = require('helmet')
 
-// DB connections
+/**
+ * Helmet functions
+ * Helmet helps you secure your Express apps by setting various HTTP headers. It's not a silver bullet, but it can help!
+ */
+app.use(helmet.expectCt());
+app.use(helmet.frameguard());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.noSniff());
+app.use(helmet.permittedCrossDomainPolicies());
+
+/**
+ * DB connections
+ */
 require('module-alias/register');
 require('@database/mongodb_conn');
 
