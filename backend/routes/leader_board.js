0526e591054f53af541235d726800938b2cd9694
Metin Ağaoğlu <metnagaoglu@gmail.com>
2022-03-24 18:25:54 +0300

    [feat] Start developing http stream and long polling solution.

--- backend/routes/leader_board.js
+++ backend/routes/leader_board.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const LeaderboardService = require('@services/leader_board');
 const asyncHandler = require('express-async-handler')
+const eventEmitter = require('@events/event_emitter');
 
 
 router.get('/', asyncHandler(async (req, res) => {
@@ -10,6 +11,26 @@ router.get('/', asyncHandler(async (req, res) => {
 	res.status(200);
 	res.json(leaderboard);
 }))
+
+
+router.get('/stream', (req, res) => {
+	res.setHeader("Content-Type","application/json; charset=utf-8")
+	res.setHeader("Transfer-Encoding","chunked")
+
+	const id = 'added.money.to.user';
+
+	eventEmitter.on(id, async(event) => {
+
+		const leaderboard = await LeaderboardService.fethScoreBoard();
+		res.write(JSON.stringify(leaderboard));
+		//res.write(JSON.stringify(event));
+	});
+
+	setTimeout(() => {
+			console.log("timer");
+			res.end();
+	}, 5000);
+});
 
 router.get('/dispatch', asyncHandler(async (req, res) => {
 
